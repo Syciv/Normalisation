44da8db1b14d0c5e2512023b6c458b3cb62970b8
Syciv <sych_iv@proitr.ru>
2022-11-13 23:01:59 +0500

    рест апи

---
+++ src/main/java/ru/film/filmopedia/controller/FilmopediaRestController.java
@@ -0,0 +1,26 @@
+package ru.film.filmopedia.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.film.filmopedia.dto.FilmMessageDto;
+import ru.film.filmopedia.service.FilmopediaService;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/films")
+public class FilmopediaRestController {
+
+    private final FilmopediaService filmopediaService;
+
+    @SneakyThrows
+    @PostMapping
+    public void save(@RequestBody FilmMessageDto filmMessageDto){
+        System.out.println(filmMessageDto);
+        filmopediaService.saveEntities(filmMessageDto.getFilmopediaDtos());
+    }
+
+}
